6994e4ec3da8350854e58c92c13fa6e6dc6efd6d
mandarkarjol <mandar.karjol@gmail.com>
2025-02-17 09:30:25 -0800

    Solution to 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    // Validate input arguments
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    };
+    if (!(num1 > 0) || !(num2 > 0)){
+        return 'ERROR';
+    };
 
+    // Order check and swap if needed
+    if (!(num2 > num1)){
+        let temp = num2;
+        num2 = num1;
+        num1 = temp;
+    }
+
+    for (num1; num1 <= num2; num1++){
+        sum += num1;
+    };
+    return sum;
 };
 
 // Do not edit below this line
