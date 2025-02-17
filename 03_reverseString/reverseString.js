530bf40b79e33b5635f58f1fb09cf44a81805161
mandarkarjol <mandar.karjol@gmail.com>
2025-02-16 20:39:34 -0800

    Solution to 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let outputString = "";
+    // Split input string into array of chars
+    let arrayOfChars = string.split("");
+    // Pop char out of the array and concatenate into a new string
+    while (arrayOfChars.length !== 0){
+        outputString += arrayOfChars.pop();
+    };
+    return outputString;
 };
 
 // Do not edit below this line
