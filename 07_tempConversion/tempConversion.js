0a402264d53fb86fe245148c79048fe572b13131
mandarkarjol <mandar.karjol@gmail.com>
2025-02-18 08:21:58 -0800

    Solution to tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  //(0°F − 32) × 5/9 = -17.78°C [Formula to convert °F to °C]
+  const tempInC = Math.round(((tempInF - 32) * (5 / 9)) * 10) / 10;
+  return tempInC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  // (0°C × 9/5) + 32 [Formula to convert °C to °F]
+  const tempInF = Math.round(((tempInC * (9 / 5) + 32)) * 10) / 10;
+  return tempInF;
 };
 
 // Do not edit below this line
