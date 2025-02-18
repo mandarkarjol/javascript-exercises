58d70e707cda05a12dbc4262924ecbdfb56531ab
mandarkarjol <mandar.karjol@gmail.com>
2025-02-18 08:36:36 -0800

    Solution to calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,47 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumValue = 0;
+  for (num of array){
+    sumValue += num;
+  };
+  return sumValue
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let multipliedValue = 1;
+  for (num of array){
+    multipliedValue *= num;
+  }
+  return multipliedValue;
 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+return num1 ** num2;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let fact = 1;
+  if (num === 0){
+    return fact;
+  }
+  else if (num === 1){
+    return fact;
+  }
+  else {
+    for (num ; num > 0 ; num--){
+      fact *= num;
+    }
+    return fact;
+  };
 	
 };
 
